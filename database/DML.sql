aba792c58f72ce457c4059815f46609a1c508a60
CV-development <carlos.valderas.a@gmail.com>
2024-08-12 01:21:53 -0400

    Primer commit

---
+++ database/DML.sql
@@ -0,0 +1,5 @@
+SELECT * FROM usuarios;
+
+INSERT INTO usuarios values
+  (DEFAULT, 'a@a.cl', 'aaa', 'admin', 'esp'),
+  (DEFAULT, 'b@b.cl', 'bbb', 'admin', 'esp');
---
+++ src/server/middlewares/auth.middleware.js
@@ -0,0 +1,17 @@
+import { jwtVerify } from '../../server/utils/auth/jwt.js'
+
+export const authToken = (req, res, next) => {
+  const authorization = req.header('Authorization')
+  if (authorization === undefined) {
+    return res.status(401).json({ message: 'Token no proporcionado' })
+  }
+  const [bearer, token] = authorization.split(' ')
+  if (bearer !== 'Bearer') {
+    return res.status(401).json({ message: 'Token mal formado' })
+  }
+  try {
+    jwtVerify(token) && next()
+  } catch (error) {
+    return res.status(401).json({ message: 'Token no válido' })
+  }
+}
